e8cf0ba6b91458b9fc4817111d36262c9f7d8089
Basim Khajwal <basimkhajwal@gmail.com>
2016-11-22 21:09:14 +0000

    Added method stubs for storage

---
+++ src/grader/storage/ProblemSetLoader.java
@@ -0,0 +1,17 @@
+package grader.storage;
+
+import grader.models.ProblemSetModel;
+
+/**
+ * Created by Basim on 22/11/2016.
+ */
+public class ProblemSetLoader {
+
+
+    public static ProblemSetModel loadProblemSet(String dirName) {
+
+        //TODO: Load the problem set
+
+        return null;
+    }
+}
---
+++ src/grader/storage/ProblemSetSaver.java
@@ -0,0 +1,14 @@
+package grader.storage;
+
+import grader.models.ProblemSetModel;
+
+/**
+ * Created by Basim on 22/11/2016.
+ */
+public class ProblemSetSaver {
+
+    public static void saveProblemSet(ProblemSetModel problemSetModel) {
+
+        // TODO: Save the given problem set
+    }
+}
