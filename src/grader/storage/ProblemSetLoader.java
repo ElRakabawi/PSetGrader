b3dad53a04027d9598ed9e3fbe9b719042c69288
Basim Khajwal <basimkhajwal@gmail.com>
2016-11-22 21:45:03 +0000

    Started adding loading code

--- src/grader/storage/ProblemSetLoader.java
+++ src/grader/storage/ProblemSetLoader.java
@@ -1,16 +1,25 @@
 package grader.storage;
 
+import grader.Constants;
 import grader.models.ProblemSetModel;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 /**
  * Created by Basim on 22/11/2016.
  */
 public class ProblemSetLoader {
 
+    public static ProblemSetModel loadProblemSet(String dirName) throws FileNotFoundException {
 
-    public static ProblemSetModel loadProblemSet(String dirName) {
+        // Look for the config file and load that
+        File configFile = new File(dirName + File.pathSeparator + Constants.MAIN_CONFIG);
+        BufferedReader configReader = new BufferedReader(new FileReader(configFile));
 
-        //TODO: Load the problem set
+        // TODO: Finish loading the problem set
 
         return null;
     }
