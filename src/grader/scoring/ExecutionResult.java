091bc823e6203649943a55aa0f4fe442599d9d9b
Basim Khajwal <basimkhajwal@gmail.com>
2016-11-27 00:28:55 +0000

    Began the scoring system design

---
+++ src/grader/scoring/ExecutionResult.java
@@ -0,0 +1,11 @@
+package grader.scoring;
+
+/**
+ * Created by Basim on 27/11/2016.
+ */
+public enum ExecutionResult {
+    AC,
+    WA,
+    TLE,
+    MLE
+}
---
+++ src/grader/scoring/Executor.java
@@ -0,0 +1,7 @@
+package grader.scoring;
+
+/**
+ * Created by Basim on 27/11/2016.
+ */
+public class Executor {
+}
